349794ba83f8a95ba4e0e123a247bf2eb8958102
Chase Corbitt <chase.corbitt.141@gmail.com>
2024-02-01 15:57:14 -0500

    update img path

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,7 +2,6 @@ import Navbar from './Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
 import PageTransitionWrapper from './PageTransitionWrapper.jsx';
-import { AnimatePresence } from 'framer-motion'
 
 export default function Layout({ location, isInitialRender }) {
 
