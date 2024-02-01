ec06131358065b2776d43a1cd7917313a4cef9ff
Chase Corbitt <chase.corbitt.141@gmail.com>
2024-02-01 15:45:40 -0500

    update navbar links

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,19 +5,19 @@ export default function Navbar() {
             <nav className="navbar">
                 <ul className="navlist">
                     <li>
-                        <NavLink to="/" className={(navData) => (navData.isActive ? 'active' : 'navlink')} ><img src="/kirbyclout.png" className="navicon"/></NavLink>
+                        <NavLink to="/portfolio" className={(navData) => (navData.isActive ? 'active' : 'navlink')} ><img src="/kirbyclout.png" className="navicon"/></NavLink>
                     </li>
                     <li>
-                        <NavLink to="/contact" className={(navData) => (navData.isActive ? 'active' : 'navlink')} >Contact</NavLink>
+                        <NavLink to="/portfolio/contact" className={(navData) => (navData.isActive ? 'active' : 'navlink')} >Contact</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/projects" className={(navData) => (navData.isActive ? 'active' : 'navlink')} >Projects</NavLink>
+                        <NavLink to="/portfolio/projects" className={(navData) => (navData.isActive ? 'active' : 'navlink')} >Projects</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/experience" className={(navData) => (navData.isActive ? 'active' : 'navlink')} >Experience</NavLink>
+                        <NavLink to="/portfolio/experience" className={(navData) => (navData.isActive ? 'active' : 'navlink')} >Experience</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/about" className={(navData) => (navData.isActive ? 'active' : 'navlink')} >About</NavLink>
+                        <NavLink to="/portfolio/about" className={(navData) => (navData.isActive ? 'active' : 'navlink')} >About</NavLink>
                     </li>
                 </ul>
             </nav>
